4a42f972e15c140930a7121ebb880ee3c90f854a
Joao Henrique <joaoadsistemas@gmail.com>
2024-01-30 12:09:29 -0300

    User Model

---
+++ entity-framework/eCommerceRelations/eCommerce.Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace eCommerce.Entities
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public char Genre { get; set; }
+        public string Rg { get; set; }
+        public string Cpf { get; set; }
+        public string MotherName { get; set; }
+        public char RegisterSituation { get; set; }
+        public DateTimeOffset RegisterTime { get; set; }
+        
+        
+        /*
+         * TODO - Vincular com as classes:
+         * - Contact
+         * - DeliverAddress
+         * - Department
+         */
+    }
+}

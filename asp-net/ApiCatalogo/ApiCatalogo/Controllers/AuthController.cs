07700fa483e80245b8f31e18a00340fda8593315
Joao Henrique <joaoadsistemas@gmail.com>
2024-02-06 13:25:39 -0300

    EndPoint Login

---
+++ asp-net/ApiCatalogo/ApiCatalogo/Controllers/AuthController.cs
@@ -0,0 +1,79 @@
+﻿using ApiCatalogo.Dtos;
+using ApiCatalogo.Entities;
+using ApiCatalogo.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace ApiCatalogo.Controllers
+{
+    [Route("/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly ITokenService _tokenService;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(ITokenService tokenService, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            _tokenService = tokenService;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] LoginDTO loginDTO)
+        {
+            var user = await _userManager.FindByNameAsync(loginDTO.Username);
+
+            if (user != null && await _userManager.CheckPasswordAsync(user, loginDTO.Password!))
+            {
+
+                var userRoles = await _userManager.GetRolesAsync(user);
+
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName!),
+                    new Claim(ClaimTypes.Email, user.Email!),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
+
+
+                foreach (var userRole in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                }
+
+                var token = _tokenService.GenerateAccessToken(authClaims, _configuration);
+
+                var refreshToken = _tokenService.GenerateRefreshToken();
+
+                _ = int.TryParse(_configuration["JWT:RefreshTokenValidityInMinutes"], out int refreshTokenValidityInMinutes);
+
+
+                user.RefreshToken = refreshToken;
+                user.RefreshTokenExpiryTime = DateTime.Now.AddMinutes(refreshTokenValidityInMinutes);
+
+                await _userManager.UpdateAsync(user);
+
+                return Ok(new
+                {
+                    Token = token,
+                    RefreshToken = refreshToken,
+                    Expiration = token.ValidTo
+                });
+
+            }
+
+            return Unauthorized();
+        }
+
+    }
+}

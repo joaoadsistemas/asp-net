b30f7f1a66e67a091f12c1ba05b7f04912e57adb
Joao Henrique <joaoadsistemas@gmail.com>
2024-02-20 17:03:50 -0300

    Initial tests

---
+++ asp-net/ApiCatalogo/ApiCatalogoxUnitTest/UnitTests/GetProdutoUnitTests.cs
@@ -0,0 +1,60 @@
+﻿using ApiCatalogo.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoxUnitTest.UnitTests
+{
+    internal class GetProdutoUnitTests : IClassFixture<ProdutosUnitTestController>
+    {
+        private readonly ProductController _controller;
+
+        public GetProdutoUnitTests(ProdutosUnitTestController controller)
+        {
+            _controller = new ProductController(controller.repository);
+        }
+
+
+
+        [Fact]
+        public async Task GetProductByIdShouldReturnProductDTO()
+        {
+
+        }
+
+
+
+        [Fact]
+        public async Task GetProductByIdShouldReturnNotFound()
+        {
+
+        }
+
+
+
+        [Fact]
+        public async Task GetProductByIdShouldReturnBadRequest()
+        {
+
+        }
+
+
+
+        [Fact]
+        public async Task GetAllProductShouldReturnListOfProductDTO()
+        {
+
+        }
+
+
+
+        [Fact]
+        public async Task GetAllProductShouldReturnBadRequest()
+        {
+
+        }
+
+    }
+}
---
+++ asp-net/ApiCatalogo/ApiCatalogoxUnitTest/UnitTests/ProdutosUnitTestController.cs
@@ -0,0 +1,33 @@
+﻿using ApiCatalogo.Repositories;
+using ApiCatalogo.Repositories.db;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoxUnitTest.UnitTests
+{
+    public class ProdutosUnitTestController
+    {
+
+        public IUnitOfWork repository;
+
+        public static DbContextOptions<SystemDbContext> dbContext { get; }
+
+        public static string connectionString = "Server=DESKTOP-ESPF03F; Database=ApiCatalagoDb; trustServerCertificate=true; Integrated Security=true";
+
+        static ProdutosUnitTestController()
+        {
+            dbContext = new DbContextOptionsBuilder<SystemDbContext>().UseSqlServer(connectionString).Options;
+        }
+
+        public ProdutosUnitTestController()
+        {
+            var context = new SystemDbContext(dbContext);
+            repository = new UnitOfWork(context);
+        }
+
+    }
+}
